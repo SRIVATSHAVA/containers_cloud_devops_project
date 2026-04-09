eff32f9c965a108ecb06f7feda9a3a9fc81c47a7
KANDASAMY Srivatshava <srivatshava.kandasamy@rntbci.com>
2026-04-09 15:41:30 +0530

    Updated sh and yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,8 @@
 
 cd /var/www/html
 
-# Remove old files
 sudo rm -rf *
 
-# Copy new files from CodeDeploy bundle
 sudo cp -r /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/* .
 
-# Restart Apache
 sudo systemctl restart httpd
